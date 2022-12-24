fc12dd6581666a21f154325120507330d6a68564
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2022-12-24 13:13:10 +0530

    add dropdown APIs

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -38,4 +38,21 @@ router.route("/id").get(async(req, res) => {
       });
 });
 
+router.route("/dropdown").get(async(req, res) => {
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query('SELECT hospitalID, name FROM hospitals',  (error, results) => {
+          if(error){
+              console.log(error)
+          }
+          else if(results.length == 0){
+              console.log("no details!!")
+          }else{
+              res.json(results)
+          }          
+        
+      });
+    });
+});
+
 module.exports = router;
