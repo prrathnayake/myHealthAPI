eb51d4083e14b0992009829776dbcb8095e7c166
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2022-12-28 14:27:57 +0530

    update ML api to take symptoms as input

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PythonShell = require('python-shell').PythonShell;
+const PythonShell = require("python-shell").PythonShell;
 const doctors = require("./routes/doctors.js");
 const patients = require("./routes/patients.js");
 const hospitals = require("./routes/hospitals.js");
@@ -10,25 +10,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/doctors', doctors);
-app.use('/patients', patients);
-app.use('/hospitals', hospitals);
-app.use('/schedules', schedules);
+app.use("/doctors", doctors);
+app.use("/patients", patients);
+app.use("/hospitals", hospitals);
+app.use("/schedules", schedules);
 
-app.get('/', (req, res) => {
-var options = {
-  scriptPath: './python',
-  args: [['itching', 'chills']]
-};
+app.get("/", (req, res) => {
+  const symptons = req.query.symptons;
 
-PythonShell.run('main.py', options, function (err, results) {
-  if (err) 
-    throw err;
-  // Results is an array consisting of messages collected during execution
-  res.json(results)
-});
+  var options = {
+    scriptPath: "./python",
+    args: [symptons],
+  };
+
+  PythonShell.run("main.py", options, function (err, results) {
+    if (err) throw err;
+    // Results is an array consisting of messages collected during execution
+    res.json(results);
+  });
 });
 
 app.listen(3001, () => {
-    console.groupCollapsed('Listening in port 3001...');
-})
+  console.groupCollapsed("Listening in port 3001...");
+});
