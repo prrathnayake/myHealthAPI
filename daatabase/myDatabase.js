674aea48ca919e0e527222e588b02a26f669eefa
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2022-12-29 10:56:02 +0530

    add env variables

--- daatabase/myDatabase.js
+++ daatabase/myDatabase.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const mysql = require("mysql2");
 
 let con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Pasan20951",
-    database: "myhealthapplication"
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PWORD,
+    database: process.env.DATABASE_DB
 });
 
 module.exports = {con};
