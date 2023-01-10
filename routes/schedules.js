11397ea54eac1aba26f3809fd2fc980eba8598c1
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-10 21:55:58 +0530

    add api to send doctor scedules

--- routes/schedules.js
+++ routes/schedules.js
@@ -82,6 +82,26 @@ router.route("/userid").get(async (req, res) => {
   });
 });
 
+router.route("/doctorid").get(async (req, res) => {
+  const id = req.query.id;
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      "SELECT * FROM schedules sc LEFT JOIN patients p on sc.patientID = p.patientID WHERE sc.staffID = ? ORDER BY sc.appointmentDate ASC",
+      [parseInt(id)],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+        } else if (results.length == 0) {
+          res.json([])
+        } else {
+          res.json(results);
+        }
+      }
+    );
+  });
+});
+
 router.route("/add").post(async (req, res) => {
   con.connect(function (err) {
     if (err) throw err;
