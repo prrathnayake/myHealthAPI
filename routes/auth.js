8adc76724aef3edf4396d7d1aca7293abd71d465
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-13 19:35:20 +0530

    add api to send role by staffID

--- routes/auth.js
+++ routes/auth.js
@@ -5,52 +5,80 @@ const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
-router.post('/login', async (req, res) => {
-  const {email, password} = req.body
-  con.connect(function(err) {
-      if (err) throw err;
-      con.query('SELECT * FROM staffs WHERE email = ?', [email], (error, results) => {
-          if(error){
-              console.log(error)
-              res.json('Somthing Wrong Please contact admin')
-          }
-          else if(results.length == 0){
-              console.log("Wrong Email!!!!!!")
-              res.json('Wrong Email!')
-          }else{
-              let spword = results[0].password
-              bcrypt.compare(password, spword, function(err, isMatch) {
-                  if (err) {
-                  console.log (err);
-                  } 
-                  else if (!isMatch) {
-                      res.json('Password doesn\'t match!')
-                  } 
-                  else {
-                      const user = { 
-                          id:results[0].staffID,
-                          email:results[0].email ,
-                          role:results[0].roleID  
-                      }
-                      const accessToken = generateAccessToken(user)
-                      res.json({accessToken: accessToken, role: results[0].roleID, authentcated: true})
-
-                  }
-              })
-          }          
-      })
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      "SELECT * FROM staffs WHERE email = ?",
+      [email],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.json("Somthing Wrong Please contact admin");
+        } else if (results.length == 0) {
+          console.log("Wrong Email!!!!!!");
+          res.json("Wrong Email!");
+        } else {
+          let spword = results[0].password;
+          bcrypt.compare(password, spword, function (err, isMatch) {
+            if (err) {
+              console.log(err);
+            } else if (!isMatch) {
+              res.json("Password doesn't match!");
+            } else {
+              const user = {
+                id: results[0].staffID,
+                email: results[0].email,
+                role: results[0].roleID,
+              };
+              const accessToken = generateAccessToken(user);
+              res.json({
+                accessToken: accessToken,
+                role: results[0].roleID,
+                id: results[0].staffID,
+                authentcated: true,
+              });
+            }
+          });
+        }
+      }
+    );
   });
-})
+});
 
-router.post('/validateToken', verfyUser,  (req, res) => {
-    res.json('authenticated')
-})
+router.post("/validateToken", verfyUser, (req, res) => {
+  res.json("authenticated");
+});
+
+router.post("/getRole", verfyUser, (req, res) => {
+  const id = req.body.id;
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query(
+      "SELECT roleID FROM staffs WHERE staffID = ?",
+      [id],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.json("Somthing Wrong Please contact admin");
+
+          res.json({ role: 0 });
+        } else {
+          res.json({
+            role: results[0].roleID,
+          });
+        }
+      }
+    );
+  });
+});
 
 // router.post('/register', async (req, res) => {
 //   console.log(req.body);
 
 //     const{ fname, lname, address, mobile, email, pword, pwordConfirm} = req.body;
-    
+
 //     con.query("SELECT email FROM staffs WHERE email = ?", [email], async (error, results) => {
 //         if(error){
 //             console.log(error)
@@ -64,7 +92,7 @@ router.post('/validateToken', verfyUser,  (req, res) => {
 //             return res.render("register", {
 //                 message: "Password don't match!!!"
 //             })
-//         } 
+//         }
 
 //         let hashedPassword = await bcrypt.hash(pword, 10);
 //         console.log(hashedPassword);
