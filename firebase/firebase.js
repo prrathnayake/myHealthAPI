ea201e3c19082d9465cd0f497cab45f218b321cd
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-13 20:10:19 +0530

    add firebase env varibale and update docker-compose

---
+++ firebase/firebase.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGESENDERID,
+  appId: process.env.FIREBASE_APPID 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
