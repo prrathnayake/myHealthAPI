1d1aac01ae37dbe9b430f0fc35ed78cc103c4074
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-13 21:11:39 +0530

    add new env variables

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./myhealth-92372-firebase-adminsdk-5llmi-3b9ac38c9e.json");
+const serviceAccount = require("./myhealth-92372-firebase-adminsdk-5llmi-3b9ac38c9e.js");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
