27b99e123e6f2d81860cc3785cac392c7cfed7fa
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-13 19:46:10 +0530

    add api to get schedule by doctor id

--- routes/patients.js
+++ routes/patients.js
@@ -38,4 +38,22 @@ router.route("/id").get(async(req, res) => {
       });
 });
 
+router.route("/doctorId").get(async(req, res) => {
+    const  id  = req.query.id
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query('SELECT * FROM patients p RIGHT JOIN schedules sc on p.patientID = sc.patientID WHERE  sc.doctorID = ?', [id],  (error, results) => {
+            if(error){
+                console.log(error)
+            }
+            else if(results.length == 0){
+                res.json([])
+            }else{
+                res.json(results)
+            }          
+          
+        });
+      });
+});
+
 module.exports = router;
