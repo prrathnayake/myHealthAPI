f3f8b70657aad86c4dd45cf35b24187f2662817e
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-15 22:58:53 +0530

    edit firebase configs

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./myhealth-92372-firebase-adminsdk-5llmi-3b9ac38c9e.js");
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(serviceAccount))
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
