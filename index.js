9815b03e40dc26bd028c7060a90e9f08294ca746
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-16 10:06:14 +0530

    add cors options

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
