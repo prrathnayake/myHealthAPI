646027150bee440f4b443b7ad1e8d717caeb1149
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-16 15:35:04 +0530

    update Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -15,20 +15,10 @@ const firebase = require("./routes/firebase.js");
 
 const app = express();
 
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  if ("OPTIONS" == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
