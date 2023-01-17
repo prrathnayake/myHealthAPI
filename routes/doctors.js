c2cfece9808c4262a01e4dc74d7248ddb5081863
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-17 22:10:47 +0530

    add api to get top 10 doctors

--- routes/doctors.js
+++ routes/doctors.js
@@ -20,6 +20,23 @@ router.route("/").get(async(req, res) => {
   });
 });
 
+router.route("/top").get(async(req, res) => {
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query('SELECT * FROM staffs WHERE roleID = 2 ORDER BY rate DESC LIMIT 10',  (error, results) => {
+          if(error){
+              console.log(error)
+          }
+          else if(results.length == 0){
+              res.json([])
+          }else{
+              res.json(results)
+          }          
+        
+      });
+    });
+  });
+
 router.route("/id").get(async(req, res) => {
     const  id  = req.query.id
     con.connect(function (err) {
